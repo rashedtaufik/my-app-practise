965ddb3f56a7c4c2def0f676f33e728419da9c8a
unknown <taufk.rashed@gmail.com>
2022-03-25 10:59:49 +0600

    done

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from '../../images/Logo.svg'
+
+const Header = () => {
+    return (
+        <nav>
+            <img src={logo} alt="" />
+            <div>
+                <a href=""></a>
+                <a href=""></a>
+                <a href=""></a>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
